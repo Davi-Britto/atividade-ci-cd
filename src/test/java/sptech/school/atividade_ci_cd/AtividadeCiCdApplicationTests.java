7b62abc6ec3c10330464f2403a9f55e182317b30
Davi-Britto <davi.britto@sptech.school>
2024-11-18 20:48:17 -0300

    first commit

---
+++ src/test/java/sptech/school/atividade_ci_cd/AtividadeCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package sptech.school.atividade_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
